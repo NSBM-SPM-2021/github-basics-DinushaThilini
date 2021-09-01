7c704af59c5a6a6cb90a07a5e7d827c05d9e7e60
DinushaThilini1009 <dinushathilini96@gmail.com>
2021-09-01 22:25:39 +0530

    c9

--- blog/src/pages/home/Home.jsx
+++ blog/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
+import { useLocation } from "react-router";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
 
 export default function Home() {
+    const location = useLocation();
+    console.log(location);
     return (
         <>
             <Header/>
