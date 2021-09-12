b743f978e32dd4c4a08cd4b1e6bb2aee2125b2e2
DinushaThilini <60134922+DinushaThilini@users.noreply.github.com>
2021-09-12 14:44:35 +0530

    update image post #5

--- blog/src/components/singlePost/SinglePost.jsx
+++ blog/src/components/singlePost/SinglePost.jsx
@@ -8,6 +8,34 @@ import { HOST } from "../../common";
 import "./singlePost.css";
 
 export default function SinglePost() {
+    const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({});
+    // const PF = "http://localhost:5000/images/";
+    const PF = IMGURL+"images/";
+  const { user } = useContext(Context);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [updateMode, setUpdateMode] = useState(false);
+
+  useEffect(() => {
+    const getPost = async ()=>{
+      const res = await axios.get(HOST+"posts/" + path);
+      setPost(res.data);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
+    };
+    getPost();
+  }, [path]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(HOST+`/posts/${post._id}`, {
+        data: { username: user.username },
+      });
+      window.location.replace("/");
+    } catch (err) {}
+  };
    const handleUpdate = async () => {
     try {
       await axios.put(HOST+`/posts/${post._id}`, {
