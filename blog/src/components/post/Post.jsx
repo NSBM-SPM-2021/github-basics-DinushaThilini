fb4e4896fdd603bcd6b55af5c88345c7dd006c4f
DinushaThilini <60134922+DinushaThilini@users.noreply.github.com>
2021-09-12 14:29:11 +0530

    Update Post.jsx

--- blog/src/components/post/Post.jsx
+++ blog/src/components/post/Post.jsx
@@ -6,24 +6,6 @@ export default function Post({ post }) {
   // const PF = "http://localhost:5000/images/";
   const PF = IMGURL+"images/";
   return (
-    <div className="post">
-      {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
-      <div className="postInfo">
-        <div className="postCats">
-          {post.categories.map((c) => (
-            <span className="postCat">{c.name}</span>
-          ))}
-        </div>
-        <Link to={`/post/${post._id}`} className="link">
-          <span className="postTitle">{post.title}</span>
-        </Link>
-        <hr />
-        <span className="postDate">
-          {new Date(post.createdAt).toDateString()}
-        </span>
-      </div>
-      <p className="postDesc">{post.desc}</p>
-    </div>
   );
 } 
 
